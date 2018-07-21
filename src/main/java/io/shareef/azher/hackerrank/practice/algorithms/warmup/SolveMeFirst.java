1d12f6d7c759a988695c06a79503abe45af66c3b
ashareef <azher.m.shareef@gmail.com>
2018-07-20 21:18:34 -0400

    add solve me first class + test

---
+++ src/main/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirst.java
@@ -0,0 +1,9 @@
+package io.shareef.azher.hackerrank.practice.algorithms.warmup;
+
+public class SolveMeFirst {
+
+    public static int solveMeFirst(int a, int b) {
+        return -1;
+    }
+
+}
---
+++ src/test/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirstTests.java
@@ -0,0 +1,14 @@
+package io.shareef.azher.hackerrank.practice.algorithms.warmup;
+
+import lombok.extern.java.Log;
+import org.junit.Test;
+
+@Log
+public class SolveMeFirstTests {
+
+    @Test
+    public void sum_of_2_and_3() {
+        
+    }
+
+}
