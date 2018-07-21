0756bed13b61d0fb9f18cb4f1f05ba522b508ea1
ashareef <azher.m.shareef@gmail.com>
2018-07-20 21:39:45 -0400

    solve me first solution

--- src/main/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirst.java
+++ src/main/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirst.java
@@ -3,7 +3,7 @@ package io.shareef.azher.hackerrank.practice.algorithms.warmup;
 public class SolveMeFirst {
 
     public static int solveMeFirst(int a, int b) {
-        return -1;
+        return a + b;
     }
 
 }
--- src/test/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirstTests.java
+++ src/test/java/io/shareef/azher/hackerrank/practice/algorithms/warmup/SolveMeFirstTests.java
@@ -1,14 +1,34 @@
 package io.shareef.azher.hackerrank.practice.algorithms.warmup;
 
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-@Log
+@Slf4j
 public class SolveMeFirstTests {
+
+    static SolveMeFirst solveMeFirst;
+
+    @BeforeClass
+    public static void setup() {
+        solveMeFirst = new SolveMeFirst();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        solveMeFirst = null;
+    }
 
     @Test
     public void sum_of_2_and_3() {
-        
+        Assert.assertEquals(5, solveMeFirst.solveMeFirst(2,3));
+    }
+
+    @Test
+    public void sum_of_100_and_1100() {
+        Assert.assertEquals(1100, solveMeFirst.solveMeFirst(100, 1000));
     }
 
 }
