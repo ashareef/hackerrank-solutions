c7130b160f48a7658241c973f1a7a08c6de4e529
ashareef <azher.m.shareef@gmail.com>
2018-07-20 20:54:54 -0400

    spring boot init

---
+++ src/main/java/io/shareef/azher/hackerrank/HackerrankApplication.java
@@ -0,0 +1,12 @@
+package io.shareef.azher.hackerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankApplication.class, args);
+	}
+}
